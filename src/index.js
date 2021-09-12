20c32e9e19fc074657260fc2957736959426321b
Gustavo Sanchez <gustavosanchez10059@gmail.com>
2021-09-11 19:07:03 -0600

    docs agregados

--- src/index.js
+++ src/index.js
@@ -7,7 +7,4 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// NODE_OPTIONS=--max_old_space_size=4096 npm run build
